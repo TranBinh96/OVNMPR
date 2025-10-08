634697f3e00ae8a9dcda54984b10d5f2babe774a
TranBinh96 <binhvinhphong@gmail.com>
2025-10-08 16:13:28 +0700

    udpate ui

--- PDFV5_forWin_for/Main.Designer.cs
+++ PDFV5_forWin_for/Main.Designer.cs
@@ -28,8 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
             this.xtraTabControl1 = new DevExpress.XtraTab.XtraTabControl();
             this.tab_nyuuryoku = new DevExpress.XtraTab.XtraTabPage();
+            this.cbDigits = new DevExpress.XtraEditors.ComboBoxEdit();
+            this.labelControl4 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl3 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl6 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl5 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.tbUnitID = new DevExpress.XtraEditors.TextEdit();
+            this.tbWorkerID = new DevExpress.XtraEditors.TextEdit();
+            this.cbStation = new DevExpress.XtraEditors.ComboBoxEdit();
+            this.cbProficiency = new DevExpress.XtraEditors.ComboBoxEdit();
             this.tab_pdf_hyouji = new DevExpress.XtraTab.XtraTabPage();
             this.im_pdf = new DevExpress.XtraEditors.PictureEdit();
             this.panel4 = new System.Windows.Forms.Panel();
@@ -40,8 +52,15 @@
             this.btnBack = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.xtraTabControl1)).BeginInit();
             this.xtraTabControl1.SuspendLayout();
+            this.tab_nyuuryoku.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.cbDigits.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbUnitID.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbWorkerID.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cbStation.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cbProficiency.Properties)).BeginInit();
             this.tab_pdf_hyouji.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.im_pdf.Properties)).BeginInit();
             this.panel4.SuspendLayout();
@@ -61,7 +80,7 @@
             this.xtraTabControl1.Location = new System.Drawing.Point(0, 0);
             this.xtraTabControl1.Name = "xtraTabControl1";
             this.xtraTabControl1.SelectedTabPage = this.tab_nyuuryoku;
-            this.xtraTabControl1.Size = new System.Drawing.Size(1126, 624);
+            this.xtraTabControl1.Size = new System.Drawing.Size(1126, 617);
             this.xtraTabControl1.TabIndex = 1;
             this.xtraTabControl1.TabPages.AddRange(new DevExpress.XtraTab.XtraTabPage[] {
             this.tab_nyuuryoku,
@@ -71,9 +90,143 @@
             // 
             this.tab_nyuuryoku.Appearance.PageClient.BackColor = System.Drawing.Color.White;
             this.tab_nyuuryoku.Appearance.PageClient.Options.UseBackColor = true;
+            this.tab_nyuuryoku.Controls.Add(this.cbDigits);
+            this.tab_nyuuryoku.Controls.Add(this.labelControl4);
+            this.tab_nyuuryoku.Controls.Add(this.labelControl3);
+            this.tab_nyuuryoku.Controls.Add(this.labelControl6);
+            this.tab_nyuuryoku.Controls.Add(this.labelControl5);
+            this.tab_nyuuryoku.Controls.Add(this.labelControl2);
+            this.tab_nyuuryoku.Controls.Add(this.labelControl1);
+            this.tab_nyuuryoku.Controls.Add(this.tbUnitID);
+            this.tab_nyuuryoku.Controls.Add(this.tbWorkerID);
+            this.tab_nyuuryoku.Controls.Add(this.cbStation);
+            this.tab_nyuuryoku.Controls.Add(this.cbProficiency);
             this.tab_nyuuryoku.Name = "tab_nyuuryoku";
-            this.tab_nyuuryoku.Size = new System.Drawing.Size(1124, 590);
+            this.tab_nyuuryoku.Size = new System.Drawing.Size(1124, 583);
             this.tab_nyuuryoku.Text = "Information Input Screen";
+            // 
+            // cbDigits
+            // 
+            this.cbDigits.Location = new System.Drawing.Point(817, 177);
+            this.cbDigits.Name = "cbDigits";
+            this.cbDigits.Properties.Appearance.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbDigits.Properties.Appearance.Options.UseFont = true;
+            this.cbDigits.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.cbDigits.Size = new System.Drawing.Size(96, 42);
+            this.cbDigits.TabIndex = 2;
+            // 
+            // labelControl4
+            // 
+            this.labelControl4.Appearance.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelControl4.Appearance.Options.UseFont = true;
+            this.labelControl4.LineLocation = DevExpress.XtraEditors.LineLocation.Center;
+            this.labelControl4.LineVisible = true;
+            this.labelControl4.Location = new System.Drawing.Point(763, 334);
+            this.labelControl4.Name = "labelControl4";
+            this.labelControl4.Size = new System.Drawing.Size(223, 36);
+            this.labelControl4.TabIndex = 1;
+            this.labelControl4.Text = "PROFICIENCY";
+            // 
+            // labelControl3
+            // 
+            this.labelControl3.Appearance.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelControl3.Appearance.Options.UseFont = true;
+            this.labelControl3.LineLocation = DevExpress.XtraEditors.LineLocation.Center;
+            this.labelControl3.LineVisible = true;
+            this.labelControl3.Location = new System.Drawing.Point(266, 343);
+            this.labelControl3.Name = "labelControl3";
+            this.labelControl3.Size = new System.Drawing.Size(142, 36);
+            this.labelControl3.TabIndex = 1;
+            this.labelControl3.Text = "STATION";
+            // 
+            // labelControl6
+            // 
+            this.labelControl6.Appearance.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelControl6.Appearance.Options.UseFont = true;
+            this.labelControl6.LineLocation = DevExpress.XtraEditors.LineLocation.Center;
+            this.labelControl6.LineVisible = true;
+            this.labelControl6.Location = new System.Drawing.Point(770, 186);
+            this.labelControl6.Name = "labelControl6";
+            this.labelControl6.Size = new System.Drawing.Size(36, 36);
+            this.labelControl6.TabIndex = 1;
+            this.labelControl6.Text = "=>";
+            // 
+            // labelControl5
+            // 
+            this.labelControl5.Appearance.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelControl5.Appearance.Options.UseFont = true;
+            this.labelControl5.LineLocation = DevExpress.XtraEditors.LineLocation.Center;
+            this.labelControl5.LineVisible = true;
+            this.labelControl5.Location = new System.Drawing.Point(916, 183);
+            this.labelControl5.Name = "labelControl5";
+            this.labelControl5.Size = new System.Drawing.Size(119, 36);
+            this.labelControl5.TabIndex = 1;
+            this.labelControl5.Text = "-DIGITS";
+            // 
+            // labelControl2
+            // 
+            this.labelControl2.Appearance.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelControl2.Appearance.Options.UseFont = true;
+            this.labelControl2.LineLocation = DevExpress.XtraEditors.LineLocation.Center;
+            this.labelControl2.LineVisible = true;
+            this.labelControl2.Location = new System.Drawing.Point(817, 37);
+            this.labelControl2.Name = "labelControl2";
+            this.labelControl2.Size = new System.Drawing.Size(120, 36);
+            this.labelControl2.TabIndex = 1;
+            this.labelControl2.Text = "UNIT ID";
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Appearance.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelControl1.Appearance.Options.UseFont = true;
+            this.labelControl1.LineLocation = DevExpress.XtraEditors.LineLocation.Center;
+            this.labelControl1.LineVisible = true;
+            this.labelControl1.Location = new System.Drawing.Point(242, 37);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(192, 36);
+            this.labelControl1.TabIndex = 1;
+            this.labelControl1.Text = "WORKER ID";
+            // 
+            // tbUnitID
+            // 
+            this.tbUnitID.Location = new System.Drawing.Point(728, 94);
+            this.tbUnitID.Name = "tbUnitID";
+            this.tbUnitID.Properties.Appearance.Font = new System.Drawing.Font("Times New Roman", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbUnitID.Properties.Appearance.Options.UseFont = true;
+            this.tbUnitID.Size = new System.Drawing.Size(308, 62);
+            this.tbUnitID.TabIndex = 0;
+            // 
+            // tbWorkerID
+            // 
+            this.tbWorkerID.Location = new System.Drawing.Point(193, 94);
+            this.tbWorkerID.Name = "tbWorkerID";
+            this.tbWorkerID.Properties.Appearance.Font = new System.Drawing.Font("Times New Roman", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbWorkerID.Properties.Appearance.Options.UseFont = true;
+            this.tbWorkerID.Size = new System.Drawing.Size(308, 62);
+            this.tbWorkerID.TabIndex = 0;
+            // 
+            // cbStation
+            // 
+            this.cbStation.Location = new System.Drawing.Point(193, 394);
+            this.cbStation.Name = "cbStation";
+            this.cbStation.Properties.Appearance.Font = new System.Drawing.Font("Times New Roman", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbStation.Properties.Appearance.Options.UseFont = true;
+            this.cbStation.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.cbStation.Size = new System.Drawing.Size(308, 62);
+            this.cbStation.TabIndex = 0;
+            // 
+            // cbProficiency
+            // 
+            this.cbProficiency.Location = new System.Drawing.Point(728, 394);
+            this.cbProficiency.Name = "cbProficiency";
+            this.cbProficiency.Properties.Appearance.Font = new System.Drawing.Font("Times New Roman", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbProficiency.Properties.Appearance.Options.UseFont = true;
+            this.cbProficiency.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.cbProficiency.Size = new System.Drawing.Size(308, 62);
+            this.cbProficiency.TabIndex = 0;
             // 
             // tab_pdf_hyouji
             // 
@@ -83,7 +236,7 @@
             this.tab_pdf_hyouji.Controls.Add(this.panel4);
             this.tab_pdf_hyouji.Controls.Add(this.panel3);
             this.tab_pdf_hyouji.Name = "tab_pdf_hyouji";
-            this.tab_pdf_hyouji.Size = new System.Drawing.Size(1124, 590);
+            this.tab_pdf_hyouji.Size = new System.Drawing.Size(1124, 583);
             this.tab_pdf_hyouji.Text = "PDF Display Screen";
             // 
             // im_pdf
@@ -92,7 +245,7 @@
             this.im_pdf.Location = new System.Drawing.Point(153, 0);
             this.im_pdf.Name = "im_pdf";
             this.im_pdf.Properties.ShowCameraMenuItem = DevExpress.XtraEditors.Controls.CameraMenuItemVisibility.Auto;
-            this.im_pdf.Size = new System.Drawing.Size(822, 590);
+            this.im_pdf.Size = new System.Drawing.Size(822, 583);
             this.im_pdf.TabIndex = 5;
             // 
             // panel4
@@ -102,7 +255,7 @@
             this.panel4.Dock = System.Windows.Forms.DockStyle.Right;
             this.panel4.Location = new System.Drawing.Point(975, 0);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(149, 590);
+            this.panel4.Size = new System.Drawing.Size(149, 583);
             this.panel4.TabIndex = 4;
             // 
             // panel5
@@ -110,7 +263,7 @@
             this.panel5.BackColor = System.Drawing.Color.White;
             this.panel5.Controls.Add(this.btnNext);
             this.panel5.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel5.Location = new System.Drawing.Point(0, 349);
+            this.panel5.Location = new System.Drawing.Point(0, 342);
             this.panel5.Name = "panel5";
             this.panel5.Size = new System.Drawing.Size(149, 241);
             this.panel5.TabIndex = 0;
@@ -131,11 +284,12 @@
             // panel3
             // 
             this.panel3.BackColor = System.Drawing.Color.White;
+            this.panel3.Controls.Add(this.button1);
             this.panel3.Controls.Add(this.panel6);
             this.panel3.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel3.Location = new System.Drawing.Point(0, 0);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(153, 590);
+            this.panel3.Size = new System.Drawing.Size(153, 583);
             this.panel3.TabIndex = 3;
             // 
             // panel6
@@ -143,7 +297,7 @@
             this.panel6.BackColor = System.Drawing.Color.White;
             this.panel6.Controls.Add(this.btnBack);
             this.panel6.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel6.Location = new System.Drawing.Point(0, 349);
+            this.panel6.Location = new System.Drawing.Point(0, 342);
             this.panel6.Name = "panel6";
             this.panel6.Size = new System.Drawing.Size(153, 241);
             this.panel6.TabIndex = 1;
@@ -167,7 +321,7 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1126, 27);
+            this.panel1.Size = new System.Drawing.Size(1126, 34);
             this.panel1.TabIndex = 2;
             // 
             // panel2
@@ -175,10 +329,23 @@
             this.panel2.BackColor = System.Drawing.Color.White;
             this.panel2.Controls.Add(this.xtraTabControl1);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel2.Location = new System.Drawing.Point(0, 27);
+            this.panel2.Location = new System.Drawing.Point(0, 34);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1126, 624);
+            this.panel2.Size = new System.Drawing.Size(1126, 617);
             this.panel2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Salmon;
+            this.button1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(0, 263);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 79);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "LAYOUT";
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // Main
             // 
@@ -187,6 +354,8 @@
             this.ClientSize = new System.Drawing.Size(1126, 651);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
+            this.IconOptions.Image = ((System.Drawing.Image)(resources.GetObject("Main.IconOptions.Image")));
+            this.IconOptions.LargeImage = ((System.Drawing.Image)(resources.GetObject("Main.IconOptions.LargeImage")));
             this.Name = "Main";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Main";
@@ -194,6 +363,13 @@
             this.Load += new System.EventHandler(this.Main_Load);
             ((System.ComponentModel.ISupportInitialize)(this.xtraTabControl1)).EndInit();
             this.xtraTabControl1.ResumeLayout(false);
+            this.tab_nyuuryoku.ResumeLayout(false);
+            this.tab_nyuuryoku.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.cbDigits.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbUnitID.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbWorkerID.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cbStation.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cbProficiency.Properties)).EndInit();
             this.tab_pdf_hyouji.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.im_pdf.Properties)).EndInit();
             this.panel4.ResumeLayout(false);
@@ -218,5 +394,17 @@
         private System.Windows.Forms.Panel panel6;
         private System.Windows.Forms.Button btnBack;
         private DevExpress.XtraEditors.PictureEdit im_pdf;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraEditors.TextEdit tbUnitID;
+        private DevExpress.XtraEditors.TextEdit tbWorkerID;
+        private DevExpress.XtraEditors.LabelControl labelControl4;
+        private DevExpress.XtraEditors.LabelControl labelControl3;
+        private DevExpress.XtraEditors.LabelControl labelControl2;
+        private DevExpress.XtraEditors.ComboBoxEdit cbDigits;
+        private DevExpress.XtraEditors.LabelControl labelControl5;
+        private DevExpress.XtraEditors.ComboBoxEdit cbStation;
+        private DevExpress.XtraEditors.ComboBoxEdit cbProficiency;
+        private DevExpress.XtraEditors.LabelControl labelControl6;
+        private System.Windows.Forms.Button button1;
     }
 }
